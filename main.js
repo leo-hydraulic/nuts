fb713e0fec43a20bd10a1ab6d5d7e74651216ca4
Leonardo Mesquita <leo@hydraulic.software>
2023-09-26 16:58:02 +0200

    Initial commit.

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  })
+
+  win.loadFile('index.html')
+  const version = app.getVersion()
+  win.webContents.executeJavaScript(`
+    const elem = document.querySelector('#app-version');
+    elem.textContent = '${version}';
+  `);
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
